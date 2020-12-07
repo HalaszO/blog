52df9751dc7ed7186540399c44f30736cd871ef8
Oliver Halasz <oliver.viktor.halasz@gmail.com>
2020-12-07 11:13:32 +0100

    First round of implementation

---
+++ query/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  } else if (type === "CommentCreated") {
+    const { postId, id, content, status } = data;
+    posts[postId].comments.push({ id, content, status });
+  } else if (type === "CommentUpdated") {
+    const { postId, id, content, status } = data;
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id === id);
+
+    comment.status = status;
+    comment.content = content;
+  }
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log("Listening on port 4002");
+});
