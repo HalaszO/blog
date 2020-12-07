c9a7fa927b786f404efd29bf9d98f4506c035940
Oliver Halasz <oliver.viktor.halasz@gmail.com>
2020-12-07 16:07:31 +0100

    Event-bus persistence added

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,22 @@ import React from "react";
 
 export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === "approved") {
+
+      content = comment.content;
+
+    } else if (comment.status === "rejected") {
+
+      content = "This comment has been moderated";
+
+    } else {
+
+      content = "Pending comment";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
